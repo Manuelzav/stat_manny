6966efe485a5d7e9b6ad8a42671ac690daa989cb
Manuel Zavala <manuelzavala@its-my-life.local>
2025-01-10 12:00:42 -0800

    length of array

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ console.log(`The odd numbers you gave are ${getOdds(numbers)}.`);
  * @returns {number} the length of the array
  */
 function getLength(numbers) {
+  return number.length;
+  
   // TODO
 }
 
